59395825603ba2bcb6ac183580f712339083aaa6
minam.project99 <minam.project99@gmail.com>
2026-01-14 17:22:50 +0100

    initial commit

---
+++ src/components/Proba.js
@@ -0,0 +1,10 @@
+import "./Proba.css"
+const Proba = () => {
+    return (
+        <>
+        <p className="novaKlasa">Hello world!</p>
+        </>
+    );
+};
+
+export default Proba;
---
+++ src/components/Test.js
@@ -0,0 +1,8 @@
+import "./Test.css"
+const Test = () => {
+    return (
+        <h1>Testni naslov iz komponente</h1>
+    )
+} 
+
+export default Test;
